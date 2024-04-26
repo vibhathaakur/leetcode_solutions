d6af246dc87df2aa856d42df220660fdb1c5cd62
vibhathaakur <thakurvibha21@gmail.com>
2024-04-26 20:09:29 +0530

    test changed

--- test.java
+++ test.java
@@ -6,9 +6,6 @@ class Solution {
             case 'V': return 5;
             case 'X': return 10;
             case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
             default: return 0;
         }
     }
