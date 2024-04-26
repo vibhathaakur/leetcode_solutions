9e30a5a0477bf6e52161e831fc4e252cc7e3e63d
vibhathaakur <thakurvibha21@gmail.com>
2024-04-26 20:07:19 +0530

    test

---
+++ test.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    private int translate(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    public int romanToInt(String s) {
+        int sum=0;
+        int curr= translate(s.charAt(0));
+
+        for(int i=1; i<s.length(); i++){
+             
+             int next= translate(s.charAt(i));
+
+             if(curr<next)
+                sum-=curr;
+    
+             else
+             sum+=curr;
+             curr=next;
+        }
+        sum+=curr;
+        return sum;
+    }
+}
